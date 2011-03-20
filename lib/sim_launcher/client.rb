a3e3b12694e6b969669ea6f2db87b4ad46c788eb
Pete Hodgson <git@thepete.net>
2011-03-20 10:35:22 -0700

    Add a SimLauncher::Client#ping command to check whether server is up

--- lib/sim_launcher/client.rb
+++ lib/sim_launcher/client.rb
@@ -26,17 +26,44 @@ module SimLauncher
     end
 
     def launch
-      full_request_uri = @server_uri.dup
-      full_request_uri.path = "/launch_#{@family}_app"
-      full_request_uri.query = "app_path=" + CGI.escape( @app_path ) + "&sdk=" + CGI.escape(@sdk)
-      puts "requesting #{full_request_uri}"
+      full_request_uri = launch_uri 
+      puts "requesting #{full_request_uri}" if $DEBUG
       response = Net::HTTP.get( full_request_uri )
-      puts "iphonesim server reponded with:\n#{response}"
+      puts "iphonesim server reponded with:\n#{response}" if $DEBUG
     end
 
     def relaunch
       launch
     end
 
+    # check that there appears to be a server ready for us to send commands to
+    def ping
+      # our good-enough solution is just request the list of available iOS sdks and
+      # check that we get a 200 response 
+      begin
+        uri = list_sdks_uri
+        Net::HTTP.start( uri.host, uri.port) do |http|
+          response = http.head(uri.path)
+          return response.is_a? Net::HTTPOK
+        end
+      rescue
+        return false
+      end
+    end
+
+    private
+
+    def launch_uri
+      full_request_uri = @server_uri.dup
+      full_request_uri.path = "/launch_#{@family}_app"
+      full_request_uri.query = "app_path=" + CGI.escape( @app_path ) + "&sdk=" + CGI.escape(@sdk)
+      full_request_uri
+    end
+
+    def list_sdks_uri
+      full_request_uri = @server_uri.dup
+      full_request_uri.path = "/showsdks"
+      full_request_uri
+    end
   end
 end
