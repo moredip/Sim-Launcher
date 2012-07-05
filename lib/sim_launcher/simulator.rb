75c5d1f27ac272102a1a42fb166009c7300063be
Pete Hodgson <git@thepete.net>
2012-07-05 08:50:05 -0700

    fail fast and verbosely when given a bad app path

--- lib/sim_launcher/simulator.rb
+++ lib/sim_launcher/simulator.rb
@@ -5,11 +5,36 @@ class Simulator
     @iphonesim_path = iphonesim_path_external || iphonesim_path(xcode_version)
   end
 
+  def check_app_path( app_path )
+    unless File.exists?( app_path )
+      return "The specified app path doesn't seem to exist:  #{app_path}"
+    end
+
+    unless File.directory? app_path
+      file_appears_to_be_a_binary = !!( `file "#{app_path}"` =~ /Mach-O executable/ )
+      if file_appears_to_be_a_binary
+        return <<-EOS
+        The specified app path is a binary executable, rather than a directory. You need to provide the path to the app *bundle*, not the app executable itself.
+        The path you specified was:               #{app_path}
+        You might want to instead try specifying: #{File.dirname(app_path)}
+        EOS
+      else
+        return "The specified app path is not a directory. You need to provide the path to your app bundle.\nSpecified app path was: #{app_path}"
+      end
+    end
+
+    nil
+  end
+
   def showsdks
     run_synchronous_command( 'showsdks' )
   end
 
   def launch_ios_app(app_path, sdk_version, device_family)
+    if problem = check_app_path( app_path )
+      bangs = '!'*80
+      raise "\n#{bangs}\nENCOUNTERED A PROBLEM WITH THE SPECIFIED APP PATH:\n\n#{problem}\n#{bangs}"
+    end
     sdk_version ||= SdkDetector.new(self).latest_sdk_version
   	run_synchronous_command( :launch, app_path, '--sdk', sdk_version, '--family', device_family, '--exit' )
   end
